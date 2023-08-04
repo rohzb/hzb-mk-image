82094d390dc71ef6f46b2c180a088179275b56cf
Ruslan Ovsyannikov <ovsyannikov@helmholtz-berlin.de>
2023-08-04 06:03:57 +0200

    fixed block  device list options in pc_info

--- hzb_pc_info.sh
+++ hzb_pc_info.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+#################################################################################
+#
+# Automatic image creation with squashfs
+#
+# Version 1.1 	- removed "-l" from lsblk, elevate privileges only once (Ruslan)
+# Version 1.0 	- basic functionality (Ruslan) 
+#
+#################################################################################
 
-sudo fdisk -l >fdisk.txt
-sudo lsblk -l >lsblk.txt
-sudo lsblk --output-all >lsblk_all.txt
-sudo lshw >lshw.txt
-sudo ip link show >ip.link.show
+# Ensure that this script runs under sudo
+if [[ $EUID -ne 0 ]];
+then
+    exec sudo /bin/bash "$0" "$@"
+fi
+
+# generate computer info
+fdisk -l >fdisk.txt
+lsblk >lsblk.txt
+lsblk --output-all >lsblk_all.txt
+lshw >lshw.txt
+ip link show >ip.link.show
 
